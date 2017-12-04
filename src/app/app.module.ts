08ee7679734d1bf2ae045568b4e73888eacd6116
Jarkko Heikkinen <jarkkohei@luukku.com>
2017-12-05 01:18:05 +0200

    Situation after the first video lecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,22 @@ import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { ItemsComponent } from './components/items/items.component';
+
+import { ItemService } from './services/item.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemsComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase, 'ItemsApp'),
     AngularFirestoreModule
   ],
-  providers: [],
+  providers: [ItemService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
