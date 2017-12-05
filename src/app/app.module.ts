ee5b4c757fca2a2de429bdb3a00dd49fc86c210e
Jarkko Heikkinen <jarkkohei@luukku.com>
2017-12-05 02:20:50 +0200

    Added the item adding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { environment } from '../environments/environment';
 
@@ -10,17 +11,22 @@ import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { ItemsComponent } from './components/items/items.component';
 
 import { ItemService } from './services/item.service';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AddItemComponent } from './components/add-item/add-item.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ItemsComponent
+    ItemsComponent,
+    NavbarComponent,
+    AddItemComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase, 'ItemsApp'),
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    FormsModule
   ],
   providers: [ItemService],
   bootstrap: [AppComponent]
--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -5,18 +5,37 @@ import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ItemService {
+
   itemsCollection: AngularFirestoreCollection<Item>;
+
   items: Observable<Item[]>;
 
   constructor(public afs: AngularFirestore) {
 
     //  Return Firestore collection named 'items' as a observable, 
     //  so that it emits the collection whenever any of its value changes.
-    this.items = this.afs.collection('items').valueChanges();
+    //this.items = this.afs.collection('items').valueChanges();
+
+    //  Set our itemsCollection as the actual Firestore collection.
+    this.itemsCollection = this.afs.collection('items');
+
+    //  Return the snapshot of the Firestore collection named 'items' as a observable. 
+    //  Map the changes and also get the id of each item in the collection.
+    this.items = this.itemsCollection.snapshotChanges().map(changes => {
+      return changes.map(a => {
+        const data = a.payload.doc.data() as Item;
+        data.id = a.payload.doc.id;
+        return data;
+      });
+    });
    }
 
    getItems() {
      return this.items;
+   }
+
+   addItem(item: Item) {
+    this.itemsCollection.add(item);
    }
 
 }
